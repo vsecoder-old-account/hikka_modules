74a9afaf246577abad52cc0bd09ecd2b2a2acfe5
Всеволод <63463168+vsecoder@users.noreply.github.com>
2022-05-28 20:57:11 +0300

    Add files via upload

--- memesgalery.py
+++ memesgalery.py
@@ -10,22 +10,15 @@
 
 """
 
-import requests
-from random import randrange
+from email import message
+import os, random, requests, imghdr
 from telethon.tl.types import Message
+from io import BytesIO
 
-from .. import loader, utils
+from .. import loader, utils, main
 from ..inline.types import InlineQuery
 
-
-async def photo() -> str:
-    return (
-        await utils.run_sync(
-            requests.get,
-            "https://meme-api.herokuapp.com/gimme",
-        )
-    ).json()['preview'][2]
-
+from telethon import TelegramClient
 
 @loader.tds
 class MemsGaleryMod(loader.Module):
@@ -33,15 +26,34 @@ class MemsGaleryMod(loader.Module):
 
     strings = {"name": "MemsGalery"}
 
-    async def client_ready(self, client, db):
+    async def client_ready(self, client: TelegramClient, db):
+        self.memes_bot = "@ffmemesbot"
+        self._db = db
         self._client = client
+
+    def generate_caption(self) -> str:
+        return 
+
+    async def photo(self) -> str:
+        async with self._client.conversation(self.memes_bot) as conv:
+            await conv.send_message("/start")
+            phtmem = await conv.get_response()
+            await conv.mark_read()
+            f = await self._client.download_media(message=phtmem, file=bytes)
+            oxo = await utils.run_sync(
+                requests.post,
+                "https://0x0.st",
+                files={"file": f},
+            )
+
+            return oxo.text
 
     async def memscmd(self, message: Message):
         """Send mems picture"""
         await self.inline.gallery(
-            caption=lambda: f"<i>{utils.ascii_face()}\n\n@vsecoder</i>",
+            caption=lambda: f'<i>{random.choice(["Dev @vsecoder", "All memes from @ffmemesbot", "Thk @skillzmeow and @shadow_hikka"])}</i>',
             message=message,
-            next_handler=photo,
+            next_handler=self.photo,
             preload=5,
         )
 
@@ -55,9 +67,9 @@ class MemsGaleryMod(loader.Module):
                 {
                     "title": "🤣 MemsGalery",
                     "description": "Send mems photo",
-                    "next_handler": photo,
-                    "thumb_handler": photo,  # Optional
-                    "caption": lambda: f"<i>{utils.ascii_face()}\n\n@vsecoder</i>",  # Optional
+                    "next_handler": self.photo,
+                    "thumb_handler": self.photo,  # Optional
+                    "caption": lambda: f'<i>{random.choice(["Dev @vsecoder", "All memes from @ffmemesbot", "Thk @skillzmeow and @shadow_hikka"])}</i>',  # Optional
                     # Because of ^ this lambda, face will be generated every time the photo is switched
                     # "caption": f"<i>Enjoy! {utils.ascii_face()}</i>",
                     # If you make it without lambda ^, it will be generated once
