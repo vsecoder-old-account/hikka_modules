98b0975d4309747267881ee12244e4b17c4d54ed
Всеволод <63463168+vsecoder@users.noreply.github.com>
2022-06-06 15:05:57 +0300

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,124 @@
+"""
+                                _             
+  __   _____  ___  ___ ___   __| | ___ _ __   
+  \ \ / / __|/ _ \/ __/ _ \ / _` |/ _ \ '__|  
+   \ V /\__ \  __/ (_| (_) | (_| |  __/ |     
+    \_/ |___/\___|\___\___/ \__,_|\___|_|     
+
+    Copyleft 2022 t.me/vsecoder                                                            
+    This program is free software; you can redistribute it and/or modify 
+
+"""
+
+__version__ = (1, 0, 0)
+
+import logging
+from math import sqrt
+from unittest import result
+from telethon import TelegramClient 
+from .. import loader
+from ..inline.types import InlineCall
+
+logger = logging.getLogger(__name__)
+
+@loader.unrestricted
+@loader.ratelimit
+@loader.tds
+class CalcMod(loader.Module):
+    """Module for inline calc"""
+
+    strings = {
+        "name": "📟 Calc",
+        "answer": ("🧮 <b>Start calculating(press inline buttons):</b>"),
+        "calc": ("🧮 <i>{0}</i>=<code>{1}</code>"),
+        "error": "❗️ Error!",
+    }
+
+    strings_ru = {
+        "answer": ("🧮 <b>Начните подсчитывать(нажимайте на инлайн кнопки):</b>"),
+        "error": "❗️ Ошибка!",
+    }
+
+    async def client_ready(self, client: TelegramClient, db):
+        self._db = db
+        self._client = client
+
+    async def return_keyboard(self, expression):
+        return [
+            #1️⃣2️⃣3️⃣4️⃣5️⃣6️⃣7️⃣8️⃣9️⃣0️⃣➗✖️➕➖🧮
+            [  
+                {"text": "1️⃣", "callback": self.calc, "args": ["1", expression]},
+                {"text": "2️⃣", "callback": self.calc, "args": ["2", expression]},
+                {"text": "3️⃣", "callback": self.calc, "args": ["3", expression]},
+            ],
+            [
+                {"text": "4️⃣", "callback": self.calc, "args": ["4", expression]},
+                {"text": "5️⃣", "callback": self.calc, "args": ["5", expression]},
+                {"text": "6️⃣", "callback": self.calc, "args": ["6", expression]},
+            ],
+            [
+                {"text": "7️⃣", "callback": self.calc, "args": ["7", expression]},
+                {"text": "8️⃣", "callback": self.calc, "args": ["8", expression]},
+                {"text": "9️⃣", "callback": self.calc, "args": ["9", expression]},
+            ],
+            [
+                {"text": "➕", "callback": self.calc, "args": ["+", expression]},
+                {"text": "0️⃣", "callback": self.calc, "args": ["0", expression]},
+                {"text": "➖", "callback": self.calc, "args": ["-", expression]},
+            ],
+            [
+                {"text": "➗", "callback": self.calc, "args": ["/", expression]},
+                {"text": "🔙", "callback": self.calc, "args": ["C", expression]},
+                {"text": "✖️", "callback": self.calc, "args": ["*", expression]},
+            ]
+        ]
+
+    async def calc(self, message: InlineCall, press, expression):
+        if expression == '0':
+            expression = ''
+        if press == "C":
+            expression = expression[:-1]
+        else:
+            expression = expression + press
+
+        a = ['+', '-', '*', '/', '=', 'C']
+        b = False
+        for i in a:
+            if press == i:
+                b = True
+
+        if b == False and expression != '' and expression != '0':
+            result = eval(expression)
+        else:
+            result = ''
+
+        text = self.strings["calc"].format(expression, result)
+
+        keyboard = await self.return_keyboard(expression)
+
+        await message.edit(
+            text=text,
+            reply_markup=keyboard
+        )
+
+    @loader.unrestricted
+    @loader.ratelimit
+    async def calccmd(self, message):
+        """
+         - init calc
+        Based on... my code)
+        """
+
+        text = self.strings["answer"]
+
+        expression = "0"
+
+        keyboard = await self.return_keyboard(expression)
+
+        await message.delete()
+        await self.inline.form(
+            text=text,
+            message=message,
+            always_allow=[message.from_id],
+            reply_markup=keyboard
+        )
